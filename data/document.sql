c5f405aae1ce8984f372cf71ce2354524cc5709f
Sia <129706771+0Medsok0@users.noreply.github.com>
2023-12-02 10:08:13 +0300

    Update document.sql

--- data/document.sql
+++ data/document.sql
@@ -1,25 +1,27 @@
 create database  Database_Practice; -- создаем базу данных Database_Practice
 \c Database_Practice; -- выбираем базу данных Database_Practice
+
 -- Создаем таблицу Skillbox 
 CREATE TABLE Skillbox (
     id SERIAL PRIMARY KEY,
     text_value TEXT
 );
+
 -- Вставляем значения
 INSERT INTO Skillbox (text_value) VALUES ('First text');
 INSERT INTO Skillbox (text_value) VALUES ('Second text');
 INSERT INTO Skillbox (text_value) VALUES ('Third text');
 
 -- Записываем в файл DDl DML команды
-$sudo -u postgres pg_dump  database_practice > skillbox_data.sql
-cat skillbox_data.sql
+$sudo -u postgres pg_dump  database_practice > data.sql
+cat data.sql
 
 -- Делаем экспорт 
-$sudo -u postgres psql -W imp < skillbox_data.sql 
+$sudo -u postgres psql -W imp < data.sql 
 $sudo -u postgres psql -- входим в бд
   \c imp;
 select * from Skillbox; -- провряем что таблица Skillbox создалась;
 
 -- Сохраняем pg_dump команды(при наличии того,что,таблица Skillbox еще не создана)
-$sudo -u postgres psql -W imp < skillbox_data.sql > commands.txt
+$sudo -u postgres psql -W imp < data.sql > commands.txt
 cat commands.txt
