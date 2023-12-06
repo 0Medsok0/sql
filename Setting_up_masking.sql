2f1e06c1f359c50461d95a64265e34df5cf6d66c
Sia <129706771+0Medsok0@users.noreply.github.com>
2023-12-06 16:30:26 +0300

    Update Setting_up_masking.sql

--- Setting_up_masking.sql
+++ Setting_up_masking.sql
@@ -1,31 +1,34 @@
 -- Создаем таблицу 
-CREATE TABLE user_test ( 
+CREATE TABLE users ( 
     id SERIAL PRIMARY KEY,
     login VARCHAR(100),
     salary NUMERIC,
     phone_number VARCHAR(15)
 );
 -- Вставляем данные
-INSERT INTO user_test (login, salary, phone_number) VALUES ('SEMIONOV', 50000, '791101234567');
-INSERT INTO user_test (login, salary, phone_number) VALUES ('DANILOV', 90000, '795301234567');
-INSERT INTO user_test (login, salary, phone_number) VALUES ('ROMANOVA', 70000, '790401234567');
+INSERT INTO users (login, salary, phone_number) VALUES ('SEMIONOV', 50000, '791101234567');
+INSERT INTO users (login, salary, phone_number) VALUES ('DANILOV', 90000, '795301234567');
+INSERT INTO users (login, salary, phone_number) VALUES ('ROMANOVA', 70000, '790401234567');
 
---Эти политики помогают контролировать доступ к данным в таблице, ограничивая тех, кто может извлекать из нее информацию, на основе их логинов.
-CREATE POLICY semionov_policy ON user_test FOR SELECT USING (login = 'SEMIONOV'); --Первая политика называется "semionov_policy" и позволяет только пользователю с именем входа "SEMIONOV" выполнять операции выбора в таблице "skillbox_test".
-CREATE POLICY danilov_policy ON user_test FOR SELECT USING (login = 'DANILOV'); --Вторая политика называется "danilov_policy" и позволяет только пользователю с логином "DANILOV" выполнять операции выбора в той же таблице "skillbox_test".
+--Эти политики помогают контролировать доступ к данным в таблице, ограничивая тех, кто может извлекать из нее информацию, на основе их логинов и текущего пользователя. 
+CREATE POLICY semionov_policy ON users FOR SELECT USING (login = 'SEMIONOV' AND current_user = 'semionov');
+CREATE POLICY danilov_policy ON users FOR SELECT USING (login = 'DANILOV' AND current_user = 'danilov');
 
 
+CREATE EXTENSION IF NOT EXISTS anon CASCADE;
+SELECT anon.start_dynamic_masking();
 
--- Настрииваем правила маскирования на таблицу user_test так, чтобы сотрудникам отдела сбыта в поле «Телефон» показывались бы только четыре символа слева, остальные символы выводились бы символом «*», сотрудники бухгалтерии должны видеть данные без маскирования (без назначения роли superuser).
+SECURITY LABEL FOR anon ON ROLE semionov IS 'MASKED';
+SECURITY LABEL FOR anon ON ROLE danilov IS 'MASKED';
 
--- Подключение расширения PostgreSQL Anonymizer
-CREATE EXTENSION IF NOT EXISTS anonymizer;
+SECURITY LABEL FOR anon ON COLUMN users.phone_number IS 'MASKED WITH FUNCTION anon.partial(phone,4,$$********$$,0)';
+\c - danilov
+SELECT * FROM skillbox_test;
+\c - semionov
+SELECT * FROM skillbox_test;
 
--- Настройка правил маскирования на таблицу user_test
-ALTER TABLE user_test 
-    ALTER COLUMN phone_number TYPE VARCHAR(15) USING 
-        CASE WHEN current_user = 'sales_department' THEN SUBSTRING(phone_number FROM 1 FOR 4) || repeat('*', length(phone_number)-4)
-        ELSE phone_number END;
-
--- Будем считать,что есть группа sales_department в которой состоят сотрудники отдела сбыта,  если это так,
---то он возьмет первые 4 символа телефонного номера с помощью функции подстроки и объединит их со строкой звездочек, сгенерированной функцией повтора, которая заменяет остальные символы в телефонном номере
+-- Эта строка кода устанавливает метку безопасности для столбца "телефон" в таблице "люди".
+--Для маскировки телефонных номеров используется функция под названием "anon.partial". 
+--В частности, там говорится, что первые четыре символа телефонного номера должны быть видны,
+--а остальные следует заменить звездочками (*). 
+-- Это метод защиты конфиденциальной информации, позволяющий при этом определенным частям данных оставаться видимыми.
